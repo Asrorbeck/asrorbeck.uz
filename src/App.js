d041691d7cc37cd35bd997126bd72238d111e826
Asrorbeck <2010288.asrorbek.tursunpulatov@gmail.com>
2024-11-25 12:36:46 +0500

    cname

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>asrorbeck v0.0.1</p>
+        <p>loading...</p>
       </header>
     </div>
   );
